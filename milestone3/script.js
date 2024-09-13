93cd16d5d861544bb925a89b1da5347b466a6b4c
touseef1khan <165935078+touseef1khan@users.noreply.github.com>
2024-09-14 00:12:25 +0500

    Add files via upload

---
+++ milestone3/script.js
@@ -0,0 +1,34 @@
+var _a;
+// listing element
+(_a = document.getElementById('form')) === null || _a === void 0 ? void 0 : _a.addEventListener('submit', function (event) {
+    event.preventDefault();
+    //type assertion
+    var nameElement = document.getElementById('name');
+    var emailElement = document.getElementById('email');
+    var phoneElement = document.getElementById('phone');
+    var addressElement = document.getElementById('address');
+    var educationElement = document.getElementById('education');
+    var experienceElement = document.getElementById('experience');
+    var skillsElement = document.getElementById('skills');
+    if (nameElement && emailElement && phoneElement && addressElement && educationElement && experienceElement && skillsElement) {
+        var name_1 = nameElement.value;
+        var email = emailElement.value;
+        var phone = phoneElement.value;
+        var address = addressElement.value;
+        var education = educationElement.value;
+        var experience = experienceElement.value;
+        var skills = skillsElement.value;
+        //create resume output
+        var resumeOutput = "\n <h2>Resume</h2>\n <p><strong>Name:</strong> ".concat(name_1, " </p>\n <p><strong>Email:</strong> ").concat(email, " </p>\n <p><strong>Phone Number:</strong> ").concat(phone, " </p>\n <p><strong>Address Number:</strong> ").concat(address, " </p>\n\n <h3>Education</h3>\n <p>").concat(education, "</p>\n\n <h3>Experience</h3>\n <p>").concat(experience, "</p>\n\n<h3>Skills</h3>\n <p>").concat(skills, "</p>\n ");
+        var resumeOutputElement = document.getElementById('resumeOutput');
+        if (resumeOutputElement) {
+            resumeOutputElement.innerHTML = resumeOutput;
+        }
+        else {
+            console.error('the resume output element are missing');
+        }
+    }
+    else {
+        console.error('one or more output element are missing');
+    }
+});
---
+++ milestone3/script.ts
@@ -0,0 +1,55 @@
+// listing element
+document.getElementById('form')?.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+
+  //type assertion
+  const nameElement = document.getElementById('name') as HTMLInputElement;
+  const emailElement = document.getElementById('email') as HTMLInputElement;
+  const phoneElement = document.getElementById('phone') as HTMLInputElement;
+  const addressElement = document.getElementById('address') as HTMLInputElement;
+  const educationElement = document.getElementById('education') as HTMLInputElement;
+  const experienceElement = document.getElementById('experience') as HTMLInputElement;
+  const skillsElement = document.getElementById('skills') as HTMLInputElement;
+
+
+  if(nameElement && emailElement && phoneElement && addressElement && educationElement && experienceElement && skillsElement){
+    
+ const name = nameElement.value
+ const email = emailElement.value
+ const phone = phoneElement.value
+ const address = addressElement.value
+ const education = educationElement.value
+ const experience = experienceElement.value
+ const skills = skillsElement.value
+
+
+ //create resume output
+ const resumeOutput = `
+ <h2>Resume</h2>
+ <p><strong>Name:</strong> ${name} </p>
+ <p><strong>Email:</strong> ${email} </p>
+ <p><strong>Phone Number:</strong> ${phone} </p>
+ <p><strong>Address Number:</strong> ${address} </p>
+
+ <h3>Education</h3>
+ <p>${education}</p>
+
+ <h3>Experience</h3>
+ <p>${experience}</p>
+
+<h3>Skills</h3>
+ <p>${skills}</p>
+ `;
+
+
+ const resumeOutputElement = document.getElementById('resumeOutput')
+ if (resumeOutputElement){
+  resumeOutputElement.innerHTML = resumeOutput
+ } else {
+   console.error('the resume output element are missing')
+ }
+  } else {
+    console.error('one or more output element are missing')
+  }
+})
